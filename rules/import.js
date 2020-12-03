e3d46d5951010ae5929f0474bbecae55fdbc14ba
rcimoszko <ryan.cimoszko@gmail.com>
2020-12-03 15:45:52 -0800

    #11668220 added eslint-import-resolver-typescript

--- rules/import.js
+++ rules/import.js
@@ -2,16 +2,20 @@ module.exports = {
     extends: ['plugin:import/errors'],
     plugins: ['import'],
     rules: {
+        'import/no-unresolved': 'error',
         'import/prefer-default-export': 'off',
         'import/no-default-export': 'error',
         'import/no-named-as-default': 'off',
         'import/no-named-as-default-member': 'off',
     },
     settings: {
-        'import/resolver': {
-            node: {
-                extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts'],
-            }
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.ts', '.tsx']
         },
+        'import/resolver': {
+            typescript: {
+                alwaysTryTypes: true
+            },
+        }
     },
 };
